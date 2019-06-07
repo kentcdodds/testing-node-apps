dfb02fefbc831fbca086ea0094a4ffad2ebda344
Kent C. Dodds <kent@doddsfamily.us>
2019-06-07 14:22:17 -0600

    make codesandbox not so sad

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+//create a server object:
+http
+  .createServer((req, res) => {
+    res.write(
+      `You may be reading this if you pull this up on codesandbox. Don't worry about it and focus on the terminal instead.`,
+    )
+    res.end()
+  })
+  .listen(8080)
